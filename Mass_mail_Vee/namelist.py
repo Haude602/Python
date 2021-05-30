f96108eefa233c5768cb758cc272db4dc17ade86
Vikrant <076bei048.vikrant@pcampus.edu.np>
2021-05-30 14:53:09 +0545

    Vee Studio Mails

---
+++ Mass_mail_Vee/namelist.py
@@ -0,0 +1,13 @@
+
+ids='''
+asinamusic01@gmail.com
+devrajparajuli38@gmail.com
+jharahul12393@gmail.com
+divansu22@gmail.com
+076bce176.suman@pcampus.edu.np
+bikendrathakurlohar143tl@gmail.com
+spramesh.shrestha@gmail.com
+dikshya18parajuli@gmail.com
+
+abhinagupta123@gmail.com
+'''
