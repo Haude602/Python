cb0816f44eed93235bf1b7774654ce2b3e0b1dc2
devpro13 <devrajparajuli38@gmail.com>
2021-05-30 17:16:28 +0545

    credentials kept in secure area and code.py file changed

--- Mass_mail_Vee/code.py
+++ Mass_mail_Vee/code.py
@@ -4,23 +4,27 @@ from email.mime.text import MIMEText   #To work with text portion of mail
 from email.mime.multipart import MIMEMultipart  # To attach imagaes or pdf or anything else
 from email.mime.base import MIMEBase  
 from email import encoders  # To encode
-from html_part import content
-from namelist import ids
-l = []
-ids=str(ids)
-#ids=ids.rstrip()
-x = ids.splitlines()
-for y in x:
-    if y not in l:
-        l.append(y)
-email_user = 'veestudioofficial@gmail.com' # The Id from which mail will be sent
-email_password = 'musicenthusiast602'  #password of ID from which mail will be sent (It is only with u in ur IDE so safe to use totally)
-count=0
-for items in l:
-    
 
+#from html_part import content
+#from namelist import ids
+from dotenv import load_dotenv
+import os
+
+with open('content.htm') as fopen:
+    """load htm content"""
+    content=fopen.read()
+whith open('namelist.txt')as fopen:
+    """load namelist ids"""
+    uids=list(set(fopen.read().splitlines()))
+
+
+load_dotenv()
+email_user = os.getenv("USERNAME") # The Id from which mail will be sent
+email_password =os.getenv("PASSWD") #password of ID from which mail will be sent (It is only with u in ur IDE so safe to use totally)
+count=0
+for id in uids:
     try:       
-        email_send = str(items) #Ask for entering E-mail address to whom mail to be sent
+        email_send = id #Ask for entering E-mail address to whom mail to be sent
         
         #paragraph=input("Enter paragraph")  #Ask for paragarph which has to be added in the middle of mail format.
                                             #It can be copied and pasted from google docs or anything
@@ -43,9 +47,7 @@ for items in l:
         
         # we will concatenate first,second and paragraph as they are still string and assign it to one variable text
 
-
-        text=content  
-        body = str(text)  #Convertion of text string again to string and assigning it to body variable
+        body = str(content)  #Convertion of text string again to string and assigning it to body variable
         msg.attach(MIMEText(body,'html'))  #Attaching this string as html script with message
 
         #filename='QR.png'  # Name of atttachment to be sent.Must be in same folder as code
